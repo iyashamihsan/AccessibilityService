935f5043edd861f6c026abea010b18eec45cd25a
Jungwoon <byjungwoon@gmail.com>
2016-10-03 17:02:20 +0900

    Initial commit

---
+++ app/src/main/java/com/byjw/accessibilityservice/MainActivity.java
@@ -0,0 +1,61 @@
+package com.byjw.accessibilityservice;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.accessibility.AccessibilityManager;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 접근성 권한이 없으면 접근성 권한 설정하는 다이얼로그 띄워주는 부분
+        if(!checkAccessibilityPermissions()) {
+            setAccessibilityPermissions();
+        }
+    }
+
+    // 접근성 권한이 있는지 없는지 확인하는 부분
+    // 있으면 true, 없으면 false
+    public boolean checkAccessibilityPermissions() {
+        AccessibilityManager accessibilityManager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+
+        // getEnabledAccessibilityServiceList는 현재 접근성 권한을 가진 리스트를 가져오게 된다
+        List<AccessibilityServiceInfo> list = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.DEFAULT);
+
+        for (int i = 0; i < list.size(); i++) {
+            AccessibilityServiceInfo info = list.get(i);
+
+            // 접근성 권한을 가진 앱의 패키지 네임과 패키지 네임이 같으면 현재앱이 접근성 권한을 가지고 있다고 판단함
+            if (info.getResolveInfo().serviceInfo.packageName.equals(getApplication().getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 접근성 설정화면으로 넘겨주는 부분
+    public void setAccessibilityPermissions() {
+        AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
+        gsDialog.setTitle("접근성 권한 설정");
+        gsDialog.setMessage("접근성 권한을 필요로 합니다");
+        gsDialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // 설정화면으로 보내는 부분
+                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                return;
+            }
+        }).create().show();
+    }
+}
